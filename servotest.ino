89169ed287769dd2bd1cf824238a4ac961df25ad
Raj Madisetti <37906311+rmadisetti3@users.noreply.github.com>
2020-10-28 14:44:31 -0400

    Update servotest.ino

--- servotest.ino
+++ servotest.ino
@@ -6,16 +6,19 @@ Created by Raj Madisetti
 #include <Servo.h> // Required by M1
 #include <Stepper.h> 
 Servo myServo;
+int pos = 0;
 //Stepper myStepper = Stepper(100, 5, 6);
 
 void setup() {
-  myServo.attach(9);
+  myServo.attach(7);
   myServo.write(0);
 }
 
 void loop() {
-  myServo.write(180);
-  delay(100);
-  myServo.write(360);
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myServo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
   //myStepper.step(500);
 }
